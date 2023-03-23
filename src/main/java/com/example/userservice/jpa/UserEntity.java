a97ae4dd547173da60c430c95289cb5e1fd6812f
eunyoung1328 <eunyeong1328@daum.net>
2023-03-23 09:36:58 +0900

    security setting

---
+++ src/main/java/com/example/userservice/jpa/UserEntity.java
@@ -0,0 +1,23 @@
+package com.example.userservice.jpa;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users2")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //sql 값 자동 증가
+    private Long id;
+
+    @Column(nullable = false, unique = true) //null 허용 안함
+    private String userId;
+    @Column(nullable = false, unique = true) //null 허용 안함
+    private String email;
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+    @Column(nullable = false, unique = true) //null 허용 안함
+    private String encrypedPwd;
+}
